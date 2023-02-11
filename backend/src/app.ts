7745e9834f30cee67abede803abdc0efcd7cb897
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2023-02-11 13:21:29 -0300

    [add]: server initialization

---
+++ backend/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application } from 'express';
+
+class Server {
+  private app: Application = express();
+
+  constructor(private readonly port: number = 4000) {
+    this.getApp()
+  }
+
+  private getApp() {
+    this.app.listen(() => {
+      console.log(`server is online on port ${this.port}`);
+    });
+  }
+}
+
+new Server();
