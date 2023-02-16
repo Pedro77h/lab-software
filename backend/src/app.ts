b7b70d0fdf3ad4ebb543d2a99b474d7b0ecc00e3
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2023-02-16 20:32:46 -0300

    [mdf]: add cors and validate entries

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,23 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 
 class Server {
   private app: Application = express();
 
   constructor(private readonly port: number = 4000) {
     this.getApp();
+    this.setup();
   }
 
-  private getApp() {
+  private getApp():void {
     this.app.listen(this.port, () => {
       console.log(`server is online on port ${this.port}`);
     });
+  }
+
+  private setup():void {
+    this.app.use(express.json());
+    this.app.use(cors());
   }
 }
 
